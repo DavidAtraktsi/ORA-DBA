b9b84441af47da4611beb9387214d15aeec6d3a2
DoodyAtraktsi <doodyatraktsi@gmail.com>
2018-01-03 16:50:19 +0200

    Adding ShrinkDatafile.sql

---
+++ ShrinkDatafile.sql
@@ -0,0 +1,9 @@
+set pages 5000
+SELECT 
+CEIL( (NVL(HWM,1)*(bytes/blocks))/1024/1024 ) SMALLEST_MB,
+CEIL( BLOCKS*(bytes/blocks)/1024/1024) CURRSIZE_MB,
+CEIL( BLOCKS*(bytes/blocks)/1024/1024) - CEIL( (NVL(HWM,1)*(bytes/blocks))/1024/1024 ) SAVINGS_MB,
+'alter database datafile '''||file_name||''' resize '||CEIL( (NVL(HWM,1)*(bytes/blocks))/1024/1024 )||'M;' ALTER_CMD
+FROM DBA_DATA_FILES DBADF, ( SELECT FILE_ID, MAX(BLOCK_ID+BLOCKS-1) HWM FROM DBA_EXTENTS GROUP BY FILE_ID ) DBAFS
+WHERE DBADF.FILE_ID = DBAFS.FILE_ID(+) 
+ORDER BY SAVINGS_MB;
