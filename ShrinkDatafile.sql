8bffa6c66f40651e8640d4419408f7cff87e9ec3
DavidAtraktsi <32240962+DoodyAtraktsi@users.noreply.github.com>
2020-10-02 15:40:06 +0300

    Update ShrinkDatafile.sql

--- ShrinkDatafile.sql
+++ ShrinkDatafile.sql
@@ -1,4 +1,5 @@
 set pages 5000
+set lines 200
 SELECT 
 CEIL( (NVL(HWM,1)*(bytes/blocks))/1024/1024 ) SMALLEST_MB,
 CEIL( BLOCKS*(bytes/blocks)/1024/1024) CURRSIZE_MB,
@@ -7,3 +8,19 @@ CEIL( BLOCKS*(bytes/blocks)/1024/1024) - CEIL( (NVL(HWM,1)*(bytes/blocks))/1024/
 FROM DBA_DATA_FILES DBADF, ( SELECT FILE_ID, MAX(BLOCK_ID+BLOCKS-1) HWM FROM DBA_EXTENTS GROUP BY FILE_ID ) DBAFS
 WHERE DBADF.FILE_ID = DBAFS.FILE_ID(+) 
 ORDER BY SAVINGS_MB;
+
+-- simpler (for terminal):
+set timing on
+
+
+
+
+set timinng on
+set pages 5000
+set lines 200
+SELECT 
+'alter database datafile '''||file_name||''' resize '||CEIL( (NVL(HWM,1)*(bytes/blocks))/1024/1024 )||'M; --'||(CEIL( BLOCKS*(bytes/blocks)/1024/1024) - CEIL( (NVL(HWM,1)*(bytes/blocks))/1024/1024 ))||' MB' ALTER_CMD
+FROM DBA_DATA_FILES DBADF, ( SELECT FILE_ID, MAX(BLOCK_ID+BLOCKS-1) HWM FROM DBA_EXTENTS GROUP BY FILE_ID ) DBAFS
+WHERE DBADF.FILE_ID = DBAFS.FILE_ID(+) 
+ORDER BY CEIL( BLOCKS*(bytes/blocks)/1024/1024) - CEIL( (NVL(HWM,1)*(bytes/blocks))/1024/1024 );
+
